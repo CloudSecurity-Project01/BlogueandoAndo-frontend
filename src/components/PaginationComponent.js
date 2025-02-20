84aaae1cff00138df05b0fc529f36d3f11810b5c
Diego Sanchez <difesanme@gmail.com>
2025-02-20 11:38:01 -0500

    Creating separate component for Pagination

---
+++ src/components/PaginationComponent.js
@@ -0,0 +1,117 @@
+import React, { useState} from "react";
+import { Pagination, Container } from "react-bootstrap";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+
+const PaginationComponent = ({ currentPage, setCurrentPage, totalPages, totalItems, pageSize, setPageSize }) => {
+
+    const [inputValue, setInputValue] = useState(pageSize);
+
+    const handlePageChange = (newPage) => {
+        if (newPage >= 1 && newPage <= totalPages) {
+            setCurrentPage(newPage);
+        }
+    };
+
+    const renderPageNumbers = () => {
+        let pages = [];
+
+        pages.push(
+            <Pagination.Item key={1} active={currentPage === 1} onClick={() => handlePageChange(1)}>
+                1
+            </Pagination.Item>
+        );
+
+        if (currentPage > 3) {
+            pages.push(<Pagination.Ellipsis key="ellipsis-start" disabled />);
+        }
+
+        if (currentPage > 2) {
+            pages.push(
+                <Pagination.Item key={currentPage - 1} onClick={() => handlePageChange(currentPage - 1)}>
+                    {currentPage - 1}
+                </Pagination.Item>
+            );
+        }
+
+        if (currentPage !== 1 && currentPage !== totalPages) {
+            pages.push(
+                <Pagination.Item key={currentPage} active>
+                    {currentPage}
+                </Pagination.Item>
+            );
+        }
+
+        if (currentPage < totalPages - 1) {
+            pages.push(
+                <Pagination.Item key={currentPage + 1} onClick={() => handlePageChange(currentPage + 1)}>
+                    {currentPage + 1}
+                </Pagination.Item>
+            );
+        }
+
+        if (currentPage < totalPages - 2) {
+            pages.push(<Pagination.Ellipsis key="ellipsis-end" disabled />);
+        }
+
+        if (totalPages > 1) {
+            pages.push(
+                <Pagination.Item key={totalPages} active={currentPage === totalPages} onClick={() => handlePageChange(totalPages)}>
+                    {totalPages}
+                </Pagination.Item>
+            );
+        }
+
+        return pages;
+    };
+
+    return (
+        <Container className="text-center mt-5">
+            <div className="d-flex justify-content-center mt-3">
+                <Pagination>
+                    <Pagination.Prev
+                        onClick={() => handlePageChange(currentPage - 1)}
+                        disabled={currentPage === 1}
+                    >
+                        <FaChevronLeft />
+                    </Pagination.Prev>
+
+                    {renderPageNumbers()}
+
+                    <Pagination.Next
+                        onClick={() => handlePageChange(currentPage + 1)}
+                        disabled={currentPage === totalPages}
+                    >
+                        <FaChevronRight />
+                    </Pagination.Next>
+                </Pagination>
+            </div>
+
+            <div className="d-flex justify-content-center mb-3 align-items-center">
+                <span>Resultados por página: </span>
+                <input
+                    type="number"
+                    className="form-control mx-2"
+                    style={{ width: "80px" }}
+                    defaultValue={pageSize}
+                    min="1"
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    onBlur={(e) => {
+                        const validValue = Math.max(1, parseInt(inputValue, 10) || 1);
+                        setPageSize(validValue);
+                        setCurrentPage(1);
+                        setInputValue(validValue);
+                    }}
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter") {
+                            e.target.blur();
+                        }
+                    }}
+                />
+                <span>de {totalItems}</span>
+            </div>
+        </Container>
+    );
+};
+
+export default PaginationComponent;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { Pagination, Dropdown, DropdownButton, ButtonGroup, Container, Button, Form, InputGroup, Badge } from "react-bootstrap";
-import { FaPlus, FaFilter, FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { Container, Button, Form, InputGroup, Badge } from "react-bootstrap";
+import { FaPlus, FaFilter } from "react-icons/fa";
 import { useAuth } from "../services/authService";
 import PostsList from "../components/PostsList";
 import PostModal from "../components/PostModal";
@@ -8,6 +8,7 @@ import { useLocation } from "react-router-dom";
 import { deletePost, getPosts } from "../services/postService";
 import "../styles/Modal.css";
 import { getTags } from "../services/tagService";
+import PaginationComponent from "../components/PaginationComponent";
 
 
 const Home = () => {
@@ -42,11 +43,11 @@ const Home = () => {
   }, [showMyPostsOnly, currentPage, pageSize]);
 
   useEffect(() => {
-      getTags()
-          .then(setAllTags)
-          .catch((error) => {
-              console.error("Error getting tags list", error)    
-          });
+    getTags()
+      .then(setAllTags)
+      .catch((error) => {
+        console.error("Error getting tags list", error)
+      });
   }, []);
 
   const handleSavePost = () => {
@@ -72,11 +73,7 @@ const Home = () => {
     filteredPosts = filteredPosts.filter(post => selectedTags.every(tag => post.tags.includes(tag)));
   }
 
-  const handlePageChange = (newPage) => {
-    if (newPage >= 1 && newPage <= totalPages) {
-      setCurrentPage(newPage);
-    }
-  };
+
 
   const handleClose = (postData) => {
     if (postData && postData.title) {
@@ -96,59 +93,11 @@ const Home = () => {
     setSelectedPost(null)
   };
 
-  const pageSizeOptions = [10, 25, 50, 100];
+  const handleShowAllTags = () => {
 
-  const renderPageNumbers = () => {
-    let pages = [];
-
-    pages.push(
-      <Pagination.Item key={1} active={currentPage === 1} onClick={() => handlePageChange(1)}>
-        1
-      </Pagination.Item>
-    );
-
-    if (currentPage > 3) {
-      pages.push(<Pagination.Ellipsis key="ellipsis-start" disabled />);
-    }
-
-    if (currentPage > 2) {
-      pages.push(
-        <Pagination.Item key={currentPage - 1} onClick={() => handlePageChange(currentPage - 1)}>
-          {currentPage - 1}
-        </Pagination.Item>
-      );
-    }
-
-    if (currentPage !== 1 && currentPage !== totalPages) {
-      pages.push(
-        <Pagination.Item key={currentPage} active>
-          {currentPage}
-        </Pagination.Item>
-      );
-    }
-
-    if (currentPage < totalPages - 1) {
-      pages.push(
-        <Pagination.Item key={currentPage + 1} onClick={() => handlePageChange(currentPage + 1)}>
-          {currentPage + 1}
-        </Pagination.Item>
-      );
-    }
-
-    if (currentPage < totalPages - 2) {
-      pages.push(<Pagination.Ellipsis key="ellipsis-end" disabled />);
-    }
-
-    if (totalPages > 1) {
-      pages.push(
-        <Pagination.Item key={totalPages} active={currentPage === totalPages} onClick={() => handlePageChange(totalPages)}>
-          {totalPages}
-        </Pagination.Item>
-      );
-    }
-
-    return pages;
   };
+
+
 
 
 
@@ -178,8 +127,8 @@ const Home = () => {
 
 
       {showFilter && (
-        <Form.Group className="my-3 position-relative">
-          <Form.Label>Filtrar por etiquetas</Form.Label>
+        <Form.Group className="my-3 position-relative d-flex align-items-center">
+          <Form.Label className="me-2 mb-0">Etiquetas:</Form.Label>
           <InputGroup>
             <Form.Control
               type="text"
@@ -192,6 +141,7 @@ const Home = () => {
               onFocus={() => setShowDropdown(true)}
               onBlur={() => setTimeout(() => setShowDropdown(false), 200)}
             />
+            <Button variant="secondary" onClick={handleShowAllTags}>Ver todas</Button>
           </InputGroup>
           {showDropdown && (
             <div className="position-absolute w-100 bg-white border rounded shadow mt-1" style={{ top: "100%", left: 0, zIndex: 1000 }}>
@@ -230,37 +180,9 @@ const Home = () => {
         handleDeletePost={handleDeletePost}
       />
 
-      <div className="d-flex justify-content-center mt-3">
-        <Pagination>
-          <Pagination.Prev
-            onClick={() => handlePageChange(currentPage - 1)}
-            disabled={currentPage === 1}
-          >
-            <FaChevronLeft />
-          </Pagination.Prev>
+      <PaginationComponent currentPage={currentPage} setCurrentPage={setCurrentPage} totalPages={totalPages} totalItems={totalItems} pageSize={pageSize} setPageSize={setPageSize}/>
 
-          {renderPageNumbers()}
 
-          <Pagination.Next
-            onClick={() => handlePageChange(currentPage + 1)}
-            disabled={currentPage === totalPages}
-          >
-            <FaChevronRight />
-          </Pagination.Next>
-        </Pagination>
-      </div>
-        
-      <div className="d-flex justify-content-center mb-3 align-items-center" >
-        <span>Resultados por página: </span>
-          <DropdownButton as={ButtonGroup} title={`${pageSize}`} variant="light" className="mx-2">
-            {pageSizeOptions.map((size) => (
-              <Dropdown.Item key={size} onClick={() => { setPageSize(size); setCurrentPage(1); }}>
-                {size}
-              </Dropdown.Item>
-            ))}
-          </DropdownButton>
-          <span>de {totalItems}</span>
-        </div>
     </Container>
   );
 };
